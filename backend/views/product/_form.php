8da501aab1d1a5ea22fcc286f3f7dbb8c5aea921
Luthfi Fauzan <luthfifauzan.inovamedika@gmail.com>
2023-10-06 11:01:11 +0700

    crud

---
+++ backend/views/product/_form.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use dosamigos\ckeditor\CKEditor;
+
+/** @var yii\web\View $this */
+/** @var common\models\Product $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->widget(CKEditor::class, [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
+
+    <?= $form->field($model, 'image', [
+        'template' => '
+                <div class="custom-file">
+                    {input}
+                    {label}
+                    {error}
+                </div>
+            ',
+        'labelOptions' => ['class' => 'custom-file-label'],
+        'inputOptions' => ['class' => 'custom-file-input']
+    ])->textInput(['type' => 'file']) ?>
+
+    <?= $form->field($model, 'price')->textInput([
+        'maxlength' => true,
+        'type' => 'number'
+    ]) ?>
+
+    <?= $form->field($model, 'status')->checkbox() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
